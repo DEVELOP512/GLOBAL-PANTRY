3bf042c05d88497c2517959a2e6a8213bed96338
DEVELOP512 <156081575+DEVELOP512@users.noreply.github.com>
2024-01-09 22:42:29 +0530

    Add files via upload
    ECOMMERCE WEBSITE

---
+++ src/data.js
@@ -0,0 +1,175 @@
+let shopItemsData = [
+  {
+  id: "obj1",
+  name:"Madras Curry Powder",
+  price: 200,
+  desc: "Indian spices combine to give the taste of Madras",
+  img: "images/MadrasCurryPowder.webp",
+
+}, 
+{
+
+  id: "obj2",
+  name:"Jamaican Curry Powder",
+  price: 500,
+  desc: "A blend of Jamaican spices, can be used in curries.",
+  img: "images/JamaicanCurryPowder.jpg",
+
+}, 
+{
+
+  id: "obj3",
+  name:"Cast-Iron Skillet",
+  price: 500,
+  desc: "Pre-seasoned skillet made of pig-iron",
+  img: "images/CastIronSkillet.jpg",
+
+}, 
+{
+
+  id: "obj4",
+  name:"Frying pan",
+  price: 350,
+  desc: "Stainless steel pan",
+  img: "images/Fryingpans.jpg",
+
+
+},
+{
+
+
+  id: "obj5",
+  name:"Ackee",
+  price: 200,
+  desc: "Jamaican Fruit",
+  img: "images/Ackee.jpg",
+
+
+},
+{
+
+
+  id: "obj6",
+  name:"Buddha's Hand",
+  price: 500,
+  desc: "A variety of citron fruit.",
+  img: "images/BuddhasHand.jpg",
+
+
+},
+{
+
+
+  id: "obj7",
+  name:"Green Zucchini",
+  price: 20,
+  desc: "The zucchini , courgette or baby marrow (Cucurbita pepo) is a summer squash.",
+  img: "images/GreenZucchini.jpg",
+
+
+},
+{
+
+
+  id: "obj8",
+  name:"Parsley",
+  price: 20,
+  desc: "French herb used for garnish",
+  img: "images/Parsley.jpg",
+
+
+},
+{
+
+
+  id: "obj9",
+  name:"Golden Seamoss",
+  price: 200,
+  desc: "It is a type of irish algae, a rich source in essential minerals and vitamins.Gold sea moss is low in calories and may help to support weight loss efforts.Gold sea moss has been traditionally used to reduce inflammation in the body and may help to alleviate symptoms of conditions such as arthritis and gout.It's important to note that sea moss should be stored in a dry place as it can become moldy if it becomes too moist. If your sea moss becomes moldy, it should be discarded and not used.",
+  img: "images/GoldenSeamoss.jpg",
+
+
+},
+{
+
+
+  id: "obj10",
+  name:"Gomasio",
+  price: 390,
+  desc: "A dry condiment made from unhulled sesame seeds.Gomasio, which translates to “sesame salt”, is a Japanese seasoning blend made from whole sesame seeds and salt.  A variation includes dried seaweed.  Gomasio is naturally gluten free and vegan.  It is a table condiment, often sprinkled over a variety of foods to add flavor and a touch of saltiness.",
+  img: "images/Gomasio.jpg",
+
+
+},
+{
+
+
+  id: "obj11",
+  name:"Salt Fat Acid Heat",
+  price: 450,
+  desc: "A reference book, the cookbook is focused on teaching techniques and structured around the four titular elements: Salt, Fat, Acid, and Heat. The book explains what they are and how to master them in cooking in order to become a better, more intuitive cook. Nosrat explains that these are the defining factors which determine the flavor and texture of food when cooking, calling them the cardinal directions of cooking.",
+  img: "images/SaltFatAcidHeat.jpg",
+
+
+},
+
+{
+
+
+  id: "obj12",
+  name:"The British Cookbook",
+  price: 100,
+  desc: "In The British Cookbook, author and food historian Ben Mervis takes readers on a mouth-watering culinary tour across England, Wales, Scotland, and Northern Ireland, revealing a cuisine as diverse as the landscape from which it originates.",
+  img: "images/TheBritishCookBook.jpg",
+
+
+},
+{
+
+
+  id: "obj13",
+  name:"MEZZALUNA",
+  price: 1200,
+  desc: "A mezzaluna is a knife consisting of one or more curved blades with a handle on each end, which is rocked back and forth chopping the ingredients below with each movement.",
+  img: "images/MEZZALUNA.jpg",
+
+
+},
+{
+
+
+  id: "obj14",
+  name:"Damascus Steel Knife",
+  price: 500,
+  desc: "From the 3rd and 4th centuries and all the way to the 18th century, Damascus steel was known for being perhaps the best type of steel for blades and knives.",
+  img: "images/Damascussteelknife.webp",
+
+
+},
+{
+
+
+  id: "obj15",
+  name:"Hollandaise Sauce",
+  price: 200,
+  desc: "Hollandaise sauce , meaning Dutch sauce in French, is a mixture of egg yolk, melted butter, and lemon juice (or a white wine or vinegar reduction). This is seasoned with salt, and white pepper.",
+  img: "images/Hollandaisesauce.jpg",
+
+
+},
+{
+
+
+  id: "obj16",
+  name:"Soya Sauce",
+  price: 50,
+  desc: "Soy sauce is a liquid condiment of Chinese origin, traditionally made from a fermented paste of soybeans, roasted grain, brine, and Aspergillus oryzae or Aspergillus sojae molds.",
+  img: "images/Soyasauce.jpg",
+
+
+},
+
+
+
+
+];
